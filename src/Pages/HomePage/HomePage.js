53c3096a3493bbc002fe4908b8df84a341caba82
Safi Saqlain <safisaqlain@gmail.com>
2020-02-29 05:46:31 +0500

    component architecture and styling menuitems

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './HomePage.scss';
+import DirectoryMenu from '../../Components/DirectoryMenu/DirectoryMenu';
+const HomePage=()=>{
+    return(<div className='homepage'>
+   <DirectoryMenu/>
+        </div>)
+}
+export default HomePage;
